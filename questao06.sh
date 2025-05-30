6cd497de0a4e6345de9344ba7b66d3d4ea3f0466
Matheus <alguemchamadomatheus@gmail.com>
2025-05-30 12:07:24 -0300

    questão 6 resolvida

--- questao06.sh
+++ questao06.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 ### Nao apague a linha acima! ###
+sudo mv ~/mais_uma_pasta/maria/ ~/mais_uma_pasta/temp; sudo mv ~/mais_uma_pasta/joao/ ~/mais_uma_pasta/maria/ ; sudo mv ~/mais_uma_pasta/temp ~/mais_uma_pasta/joao/
